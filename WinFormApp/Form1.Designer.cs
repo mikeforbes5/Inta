62ef46a2c89f96a80ac31ae647cc2d623adbd169
mikeforbes5 <32751573+mikeforbes5@users.noreply.github.com>
2019-04-01 15:31:25 -0500

    Add files via upload

---
+++ WinFormApp/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApp15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetCredentialsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(49, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Execute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 124);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 186);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(46, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Vendor CAID";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(726, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetCredentialsToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // resetCredentialsToolStripMenuItem
+            // 
+            this.resetCredentialsToolStripMenuItem.Name = "resetCredentialsToolStripMenuItem";
+            this.resetCredentialsToolStripMenuItem.Size = new System.Drawing.Size(164, 22);
+            this.resetCredentialsToolStripMenuItem.Text = "Reset Credentials";
+            this.resetCredentialsToolStripMenuItem.Click += new System.EventHandler(this.resetCredentialsToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp15.Properties.Resources.ezgif_com_resize1;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 392);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(56, 50);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "EDI",
+            "IDC",
+            "CDS"});
+            this.comboBox1.Location = new System.Drawing.Point(206, 70);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.Text = "Choose Vendor";
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(206, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "North America Event";
+            this.label2.Visible = false;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Yes",
+            "No"});
+            this.comboBox2.Location = new System.Drawing.Point(209, 124);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(61, 21);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.Text = "Choose";
+            this.comboBox2.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(726, 454);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetCredentialsToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ WinFormApp/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using Oracle.ManagedDataAccess.Client;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+
+
+
+
+namespace WindowsFormsApp15
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+
+
+            string path = @"C:\LazyPull\FeedPull\Username.txt";
+            string path2 = @"C:\LazyPull\FeedPull\Password.txt";//path to resource file location
+            InitializeComponent();
+
+            if (File.Exists(path) == false && File.Exists(path2) == false)
+            {
+
+                Form Form2 = new Form2();
+                textBox1.Visible = false;
+                textBox2.Visible = false;
+                button1.Visible = false;
+                label1.Visible = false;
+                Form2.Show();
+            }
+            
+        }
+        private void comboBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+            string value = "";
+            comboBox1.Invoke(new MethodInvoker(delegate
+            {
+                if (comboBox1.SelectedIndex != -1)
+                {
+                    value = comboBox1.SelectedItem.ToString();
+                }
+            }));
+            if (value == "IDC")
+            {
+                label2.Visible = true;
+                comboBox2.Show();
+            }
+            else if (value == "EDI")
+            {
+                label2.Visible = false;
+                comboBox2.Hide();
+            }
+            else if (value == "CDS")
+            {
+                label2.Visible = false;
+                comboBox2.Hide();
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Show();
+            backgroundWorker1.RunWorkerAsync();
+
+        }
+
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+
+        private void resetCredentialsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form Form2 = new Form2();
+            textBox1.Visible = false;
+            textBox2.Visible = false;
+            button1.Visible = false;
+            label1.Visible = false;
+            Form2.Show();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+        }
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+
+
+                OracleConnection cnn;
+                string sql = null;
+                string data = null;
+
+                int i = 0;
+                int j = 0;
+
+                Excel.Application xlApp;
+                Excel.Workbook xlWorkBook;
+                Excel.Worksheet xlWorkSheet;
+                object misValue = System.Reflection.Missing.Value;
+
+                xlApp = new Excel.Application();
+                xlWorkBook = xlApp.Workbooks.Add(misValue);
+                xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+
+                Excel.Workbook MyBook = null;
+                Excel.Application MyApp = null;
+                Excel.Worksheet MySheet = null;
+                MyApp = new Excel.Application();
+                //MyBook = MyApp.Workbooks.Add(misValue);
+                MyBook = MyApp.Workbooks.Open(@"C:\LazyPull\FeedPull\Raw_Feed_Template_V4.xlsm");
+                MySheet = (Excel.Worksheet)MyBook.Worksheets.get_Item(2);
+                MySheet.Cells.ClearContents();
+                MySheet.Columns.NumberFormat = "General";
+                //string path = @"C:\LazyPull\FeedPull\Username.txt";
+                //string path2 = @"C:\LazyPull\FeedPull\Password.txt";
+                string Username = System.IO.File.ReadAllLines(@"C:\LazyPull\FeedPull\Username.txt").First();
+                string Password = System.IO.File.ReadAllLines(@"C:\LazyPull\FeedPull\Password.txt").First();
+                string connectionString = $"Data Source= (DESCRIPTION =(ADDRESS = (PROTOCOL = TCP)(HOST = mcadb01.markit.partners)(PORT = 1521)) (CONNECT_DATA = (SERVICE_NAME = MCASERV.markit.partners))));User Id={Username};Password={Password}";
+                cnn = new OracleConnection(connectionString);
+                cnn.Open();
+
+                string caid = textBox2.Text;
+
+                try
+                {
+                    string value = "";
+                    comboBox1.Invoke(new MethodInvoker(delegate {
+                        if (comboBox1.SelectedIndex != -1)
+                        {
+                            value = comboBox1.SelectedItem.ToString();
+                        }
+                    }));
+                    if (value == "EDI")
+                    {
+                        textBox1.Text = @"select distinct a.vndr_nm,a.FFID, a.starttime, b.* from
+(select distinct t21.vndr_nm, t564.caid, t564.FFID, trunc(ff.starttime) as starttime from CORE_MCA.NORMALIZE_DS_COMPOSITE_MAPPING tn, CORE_MCA.TBGCA21_VNDR_RCRD t21, CORE_FEED_CASTLE.RAW_MT564_EDI_EVENT t564, feedfile ff
+where tn.NORMALIZE_ID = t21.VNDR_NTC_ID
+and t564.caid = substr(vndr_ca_id, 0, 16)
+and ff.pkey = t564.ffid
+and t21.vndr_ntc_id ='" + caid + @"') a,
+(select t564.caid, t564.field_path, t564.tag_name, t564.raw_key, t564.raw_value from CORE_FEED_CASTLE.RAW_MT564_EDI_EVENT t564) b
+where a.caid = b.caid
+
+
+UNION
+
+select distinct a.vndr_nm,a.FFID, a.starttime, b.* from
+(select distinct t21.vndr_nm, t564.caid, t564.FFID, trunc(ff.starttime) as starttime from CORE_MCA.NORMALIZE_DS_COMPOSITE_MAPPING tn, CORE_MCA.TBGCA21_VNDR_RCRD t21, CORE_FEED_CASTLE.RAW_MT564_EDI_EVENT t564, feedfile ff
+where tn.NORMALIZE_ID = t21.VNDR_NTC_ID
+and t564.caid = substr(vndr_ca_id, 0, 16)
+and ff.pkey = t564.ffid
+and t21.vndr_ntc_id = '" + caid + @"') a,
+(select t564.caid, t564.field_path, t564.tag_name, t564.raw_key, t564.raw_value from CORE_FEED_CASTLE.RAW_MT564_EDI_OPTION t564) b
+where a.caid = b.caid
+
+
+UNION
+
+select distinct a.vndr_nm,a.FFID, a.starttime, b.* from
+(select distinct t21.vndr_nm, t564.caid, t564.FFID, trunc(ff.starttime) as starttime from CORE_MCA.NORMALIZE_DS_COMPOSITE_MAPPING tn, CORE_MCA.TBGCA21_VNDR_RCRD t21, CORE_FEED_CASTLE.RAW_MT564_EDI_EVENT t564, feedfile ff
+where tn.NORMALIZE_ID = t21.VNDR_NTC_ID
+and t564.caid = substr(vndr_ca_id, 0, 16)
+and ff.pkey = t564.ffid
+and t21.vndr_ntc_id = '" + caid + @"') a,
+(select t564.caid, t564.field_path, t564.tag_name, t564.raw_key, t564.raw_value from CORE_FEED_CASTLE.RAW_MT564_EDI_PAYOUT t564) b
+where a.caid = b.caid
+order by starttime asc
+";
+                    }
+                    else if (value == "IDC")
+                    {
+                        textBox1.Text = @"select distinct a.vndr_nm,a.FFID, a.starttime, b.* from 
+(select distinct t21.vndr_nm, t564.caid, t564.FFID, trunc(ff.starttime) as starttime from CORE_MCA.NORMALIZE_DS_COMPOSITE_MAPPING tn, CORE_MCA.TBGCA21_VNDR_RCRD t21, CORE_FEED_CASTLE.RAW_IDC_EVENTS t564, feedfile ff
+where tn.NORMALIZE_ID = t21.VNDR_NTC_ID
+and t564.caid = substr(vndr_ca_id,0,10)
+and ff.pkey = t564.ffid
+and t21.vndr_ntc_id = '" + caid + @"') a,
+(select t564.caid, t564.field_path, t564.local_name, t564.field_name, t564.field_value from CORE_FEED_CASTLE.RAW_IDC_EVENTS t564) b
+where a.caid = b.caid  
+
+UNION
+
+select distinct a.vndr_nm,a.FFID, a.starttime, b.* from 
+(select distinct t21.vndr_nm, t564.caid, t564.FFID, trunc(ff.starttime) as starttime from CORE_MCA.NORMALIZE_DS_COMPOSITE_MAPPING tn, CORE_MCA.TBGCA21_VNDR_RCRD t21, CORE_FEED_CASTLE.RAW_IDC_EVENTS t564, feedfile ff
+where tn.NORMALIZE_ID = t21.VNDR_NTC_ID
+and t564.caid = substr(vndr_ca_id,0,10)
+and ff.pkey = t564.ffid
+and t21.vndr_ntc_id = '" + caid + @"') a,
+(select t564.caid, t564.field_path, t564.local_name, t564.field_name, t564.field_value from CORE_FEED_CASTLE.RAW_IDC_OPTIONS t564) b
+where a.caid = b.caid
+
+UNION
+
+select distinct a.vndr_nm,a.FFID, a.starttime, b.* from 
+(select distinct t21.vndr_nm, t564.caid, t564.FFID, trunc(ff.starttime) as starttime from CORE_MCA.NORMALIZE_DS_COMPOSITE_MAPPING tn, CORE_MCA.TBGCA21_VNDR_RCRD t21, CORE_FEED_CASTLE.RAW_IDC_EVENTS t564, feedfile ff
+where tn.NORMALIZE_ID = t21.VNDR_NTC_ID
+and t564.caid = substr(vndr_ca_id,0,16)
+and ff.pkey = t564.ffid
+and t21.vndr_ntc_id = '" + caid + @"') a,
+(select t564.caid, t564.field_path, t564.local_name, t564.field_name, t564.field_value from CORE_FEED_CASTLE.RAW_IDC_PAYOUTS t564) b
+where a.caid = b.caid
+order by starttime asc";
+                    }
+                    else
+                        throw new NotImplementedException();
+                }   
+                
+                catch (NotImplementedException)
+                {
+                    MessageBox.Show("Please select vendor name");
+                    
+                }
+
+                sql = textBox1.Text;
+
+                OracleDataAdapter dscmd = new OracleDataAdapter(sql, cnn);
+                DataSet ds = new DataSet();
+                DataSet dss = new DataSet();
+                OracleCommand cmd = new OracleCommand(sql, cnn);
+                cmd.BindByName = true;
+                OracleDataReader reader = cmd.ExecuteReader();
+                DataTable schemaTable = reader.GetSchemaTable();
+                dscmd.Fill(ds);
+
+                int ii = 0;
+                for (j = 0; j <= ds.Tables[0].Columns.Count - 1; j++)
+
+                {
+
+                    DataRow row = schemaTable.Rows[ii];
+                  // xlWorkSheet.Cells[i + 1, j + 1] = row["columnName"];
+                    MySheet.Cells[i + 1, j + 1] = row["columnName"];
+                    ii = ii + 1;
+
+                }
+
+                for (i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+                {
+
+                    for (j = 0; j <= ds.Tables[0].Columns.Count - 1; j++)
+                    {
+
+                        data = ds.Tables[0].Rows[i].ItemArray[j].ToString();
+                       // xlWorkSheet.Cells[i + 2, j + 1] = data;
+                        MySheet.Cells[i + 2, j + 1] = data;
+                    }
+
+                }
+               if (MySheet.Cells[2,2].value  == null)
+               {
+                   xlWorkBook.Close(false, misValue, misValue);
+                xlApp.Quit();
+                 MyBook.Close(false, misValue, misValue);
+                  MyApp.Quit();
+                 releaseObject(MySheet);
+                  releaseObject(MyBook);
+                releaseObject(MyApp);
+                 MessageBox.Show("please pick a valid VNDR_NTC_ID");
+                 return;
+                }
+                MyApp.Visible = true;
+                MyApp.Run("Dist");
+
+               // xlWorkBook.SaveAs("Raw_Feed_Pull.xls", Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                xlWorkBook.Close(true, misValue, misValue);
+                xlApp.Quit();
+
+                releaseObject(xlWorkSheet);
+                releaseObject(xlWorkBook);
+                releaseObject(xlApp);
+
+
+               // MessageBox.Show("Excel file created , you can find the file c:\\Raw_Feed_Pull.xls");
+
+                releaseObject(MySheet);
+                releaseObject(MyBook);
+                releaseObject(MyApp);
+
+
+
+
+            }
+            catch (InvalidOperationException)
+            {
+                { };
+            }
+
+            catch (OracleException)
+            {
+                MessageBox.Show("Please Input Valid Credentials");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+
+            finally
+            {
+
+                GC.Collect();
+            }
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            pictureBox1.Hide();
+        }
+
+
+    }
+
+   }
+
+    
+   
+ 
